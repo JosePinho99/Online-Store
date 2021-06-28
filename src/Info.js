3aae690c93c8ccee91a887163b440f96aab57ea0
José Pinho <63501017+JosePinho99@users.noreply.github.com>
2021-06-28 04:17:14 +0100

    Update Info.js

--- src/Info.js
+++ src/Info.js
@@ -4,7 +4,7 @@ function Info() {
     return (
         <div id="info">
             <div id="text-container">
-                <p id="info-text">A Tabela Permanente é uma loja dedicada à venda de materias de construção</p>
+                <p id="info-text">A Tabela Permljgkljkanente é uma loja dedicada à venda de materias de construção</p>
                 <p id="info-text">Aqui encontrará todo o tipo de produtos que precisar ao melhor preço</p>
             </div>
         </div>
